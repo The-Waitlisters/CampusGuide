84cf2bb88882a87cc7e0f8911fb1a50b8c551984
Malcolm <malcolm@shredsauce.com>
2026-02-02 22:11:35 -0500

    Fix to PR #42. main.dart had not updated.

--- lib/main.dart
+++ lib/main.dart
@@ -21,22 +21,3 @@ class CampusGuideApp extends StatelessWidget {
     );
   }
 }
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('The Waitlisters'),
-      ),
-      body: const Center(
-        child: Text(
-          'Campus Guide is running',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
