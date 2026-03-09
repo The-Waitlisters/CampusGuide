fcbaa3fa93bcc65d89abc93704729fc45b2a325e
Hugo Moslener <hugo.moslener@gmail.com>
2026-03-08 23:38:36 -0400

    fixed merge issues

--- test/widgets/home/map_layer_test.dart
+++ test/widgets/home/map_layer_test.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
