363cf7af2b9fd8c4f6f55116de9e80ed7f239d5d
Owen <jtowen20@gmail.com>
2026-03-24 22:46:42 -0400

    added tests for poi_detail_sheet class/widget

--- test/widgets/home/poi_detail_test.dart
+++ test/widgets/home/poi_detail_test.dart
@@ -5,6 +5,7 @@ import 'package:proj/models/campus.dart';
 import 'package:proj/models/campus_building.dart';
 import 'package:proj/models/poi.dart';
 import 'package:proj/widgets/home/poi_detail_content.dart';
+import 'package:proj/widgets/home/poi_detail_sheet.dart';
 
 Poi testPoi({
   String id = '',
@@ -35,7 +36,20 @@ PoiDetailContent testPOIContent() {
   );
 }
 
-Widget buildPOI() {
+PoiDetailSheet testPOISheet() {
+  return PoiDetailSheet(
+      building: testPoi(id: '01'),
+      startBuilding: testBuilding(id: '1'),
+      endBuilding: testBuilding(id: '2'),
+      startPoi: testPoi(id: '02'),
+      endPoi: testPoi(id: '03'),
+      onSetStart: () {},
+      onSetDestination: () {},
+      onViewIndoorMap: () {},
+  );
+}
+
+Widget buildPOIDC() {
   return MaterialApp(
     home: Scaffold(
       body: testPOIContent(),
@@ -43,36 +57,54 @@ Widget buildPOI() {
     );
 }
 
+Widget buildPOIDS() {
+  return MaterialApp(
+    home: Scaffold(
+      body: testPOISheet(),
+    ),
+  );
+}
+
 void main() {
   group('POI detail contents', () {
-
     testWidgets('Check the Header of POI detail contents', (WidgetTester tester) async {
-      Widget testPoiDC = buildPOI();
+      Widget testPoiDC = buildPOIDC();
       await tester.pumpWidget(testPoiDC);
 
       expect(find.text("POI01 - POI01"), findsOneWidget);
     });
 
     testWidgets('Check the inner Section of POI detail contents', (WidgetTester tester) async {
-      Widget testPoiDC = buildPOI();
+      Widget testPoiDC = buildPOIDC();
       await tester.pumpWidget(testPoiDC);
 
       expect(find.text("Opening Hours:"), findsOneWidget);
     });
 
     testWidgets('Check if \'Set as Destination\' button is there for POI detail contents', (WidgetTester tester) async {
-      Widget testPoiDC = buildPOI();
+      Widget testPoiDC = buildPOIDC();
       await tester.pumpWidget(testPoiDC);
 
       expect(find.text("Set as Destination"), findsOneWidget);
     });
 
     testWidgets('Check if the indoor view button is there for POI detail contents', (WidgetTester tester) async {
-      Widget testPoiDC = buildPOI();
+      Widget testPoiDC = buildPOIDC();
       await tester.pumpWidget(testPoiDC);
 
       expect(find.text("View indoor map"), findsOneWidget);
     });
+  });
+
+  group('POI detail sheets', () {
+    testWidgets('Check the widgets POI detail sheet', (WidgetTester tester) async {
+      Widget testPoiDS = buildPOIDS();
+      await tester.pumpWidget(testPoiDS);
+
+      expect(find.byType(PoiDetailContent), findsOneWidget);
+    });
+
+
 
   });
 }
